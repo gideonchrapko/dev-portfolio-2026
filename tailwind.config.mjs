857ad8e0b909cdcdb2339e0a024c7a76c74bfc22
Gideon Chrapko <gideonchrapko@Mac.hitronhub.home>
2026-02-22 23:09:48 -0500

    adding front page ui

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'light-green': '#EFFFC1',
+        footer: '#FAFAFA',
+      },
+      fontFamily: {
+        sans: [
+          'ui-monospace',
+          'SFMono-Regular',
+          '"SF Mono"',
+          'Menlo',
+          'Consolas',
+          '"Liberation Mono"',
+          'monospace',
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
